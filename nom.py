fd36a88067acfda4533a598c097b0b1ec7f37c4f
canisius <canisiusnobre@gmail.com>
2026-07-11 19:55:26 +0100

    changement apporté dans l'arrangement du répertoire

--- nom.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-
-print("Nom du PC :", hostname)
-print("Adresse IP :", ip_address)
-
--- ollama_use.py
+++
@@ -1,7 +0,0 @@
-from ollama import chat
-
-response = chat(
-    model='qwen2.5:0.5b',
-    messages=[{'role': 'user', 'content': 'Hello!'}],
-)
-print(response.message.content)
