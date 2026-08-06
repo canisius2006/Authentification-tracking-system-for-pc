06d611a7192a912ab7e3e67bf0fdc1d28b37e843
Canisius Nobre <canisiusnobre@gmail.com>
2026-08-06 22:59:46 +0100

    Update Celery broker and result backend URLs

--- authentification/authentification/settings.py
+++ authentification/authentification/settings.py
@@ -164,9 +164,9 @@ USE_TZ = True
 
 #Configuration de celery
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = "redis://127.0.0.1:6379"
 
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379"
 
 CELERY_ACCEPT_CONTENT = [
     "json",
